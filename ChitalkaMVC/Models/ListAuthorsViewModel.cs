76eff0c9e39f359886b52574b38e8826694212ff
Kokkibegushidoktor <tempml08@gmail.com>
2022-05-17 00:08:17 +0300

    Books edit view now shows current author, century and genres values

---
+++ ChitalkaMVC/Models/ListAuthorsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ChitalkaMVC.Models
+{
+    public class ListAuthorsViewModel
+    {
+        public int AuthorId { get; set; }
+        public SelectList Authors { get; set; }
+    }
+}
---
+++ ChitalkaMVC/Models/ListCenturiesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ChitalkaMVC.Models
+{
+    public class ListCenturiesViewModel
+    {
+        public int CenturyId { get; set; }
+        public SelectList Centuries { get; set; }
+    }
+}
