d7f2ea45c5214efa6ef5d3b4adad048a9554137f
Kokkibegushidoktor <tempml08@gmail.com>
2022-05-20 00:43:42 +0300

    iii

---
+++ ChitalkaMVC.Logic/UserBooks/IUserBookManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ChitalkaMVC.Logic.UserBooks
+{
+    public interface IUserBookManager
+    {
+        Task Create(string username, int bookId);
+        Task Delete(string username, int bookId);
+    }
+}
---
+++ ChitalkaMVC.Logic/UserBooks/UserBookManager.cs
@@ -0,0 +1,27 @@
+﻿namespace ChitalkaMVC.Logic.UserBooks
+{
+    public class UserBookManager : IUserBookManager
+    {
+        private readonly ChitalkaContext _context;
+        public UserBookManager(ChitalkaContext context)
+        {
+            _context = context;
+        }
+        public async Task Create(string username, int bookId)
+        {
+            var item = new UserBook { UserId = username, BookId = bookId };
+            _context.UserBook.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(string username, int bookId)
+        {
+            var item = _context.UserBook.FirstOrDefault(ub => ub.BookId == bookId && ub.UserId == username);
+            if (item != null)
+            {
+                _context.UserBook.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
